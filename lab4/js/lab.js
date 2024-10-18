d949901f3d8e768df58c735d2182a71887ea8d03
maiwashburn <mkhwashburn@gmail.com>
2024-10-17 20:34:12 -0700

    Javascript and html lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudocode
+// Author: Maia Washburn
+// Date: October 17, 2024
 
 // Constants
 
@@ -19,3 +19,20 @@ function main() {
 
 // let's get this party started
 main();
+
+//Look for references
+//Open drawing program
+  //Choose brushes and colors
+  //Sketch
+  //Sketch again! and modify
+//Draw line art
+  //Add shadows and highlights
+  //Add finishing details
+//Export!
+
+//Create buttons for Rock, Paper, and Scissor actions
+  //Name these buttons
+//Create randomizer with Bot
+  //Set radomizer to choose winning action based off of random odds
+  //Set win conditions (when button is pressed)
+//Print and show the results of game
