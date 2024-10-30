c87f786f78d0f8613f1a09df2e80f9f00e2c82be
maiwashburn <mkhwashburn@gmail.com>
2024-10-29 22:20:25 -0700

    lab 8

---
+++ lab8/js/lab8.js
@@ -0,0 +1,27 @@
+// index.js - Lab 8: Anon Functions and Callbacks
+// Author: Maia Washburn
+// Date: October 29, 2024
+// 
+
+
+//
+
+//
+function addArray(x, y) {
+    return (x + 4);
+}
+
+//test fumction
+console.log("Add 2 and 4", addArray(2, 4));
+
+array = [2, 5, 7, 4, 9]
+console.log("My array", array);
+
+var result = array.map(addArray);
+console.log("Test adding 4 to array:", result);
+
+var result = array.map(function(x, y) {
+    return x * 5;
+})
+
+console.log("Multiplied by 5 of array:", result);
